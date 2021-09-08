6cfd2bb8e4192df70755d29ae49c3b03ea6ac38f
Sylvain Lannuzel <sylvain.lannuzel@cybeletech.com>
2021-09-08 17:37:38 +0200

    typo

--- slurm/extrapolation_77.py
+++ slurm/extrapolation_77.py
@@ -21,9 +21,9 @@ def make_slurm(exp_name, job_name_short, job_name_long, seq_len, d_model, val_co
 if __name__ == "__main__":
     d_model = [16, 64]
     seq_len = [10, 30]
-    val_contiguous = [True, False]
+    val_contiguous = [False]
     for i, (d, s, v) in enumerate(itertools.product(d_model, seq_len, val_contiguous)):
-        exp_name = f"77_{d}_{s}_val_c" if v else f"77_{d_model}_{seq_len}_random_val"
+        exp_name = f"77_{d}_{s}_val_c" if v else f"77_{d}_{s}_random_val"
         job_name_long = f"slurm/extrapolation_77_{i}.slurm"
         make_slurm(exp_name=exp_name, job_name_short="extra77", job_name_long=job_name_long, seq_len=s, d_model=d, val_contiguous=v)
         os.system(f"sbatch {job_name_long}")
