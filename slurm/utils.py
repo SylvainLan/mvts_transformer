ae8c156e5bc4baed7f79f362f8bd7d1a7f6b2c45
Sylvain Lannuzel <sylvain.lannuzel@cybeletech.com>
2021-09-09 09:48:21 +0200

    imputation files

--- slurm/utils.py
+++ slurm/utils.py
@@ -1,20 +1,32 @@
 import os
 
-def start_script(job_name):
+def start_script(job_name, mode="cpu"):
+    if mode == "cpu":
+        partition = "cpu_long"
+        cpu_per_task = 4
+        conda_load = ""
+        gpu_load = ""
+    elif mode == "gpu":
+        partition = "gpu"
+        cpu_per_task = 2
+        conda_load = "module load cuda/10.2.89/intel-19.0.3.199\n"
+        gpu_load = "#SBATCH --gres=gpu:1\n"
+    else:
+        raise AttributeError("soit gpu soit cpu")
+
     command = ("#!/bin/bash\n" +
                 f"#SBATCH --job-name={job_name}\n" +
                 "#SBATCH --output=output_ruche/%x.o%j\n" +
                 "#SBATCH --time=24:00:00\n" +
                 "#SBATCH --ntasks=1\n" +
-                "#SBATCH --cpus-per-task=4\n"
+                f"#SBATCH --cpus-per-task={cpu_per_task}\n"
                 "#SBATCH --mem=64GB\n" +
-                # "#SBATCH --gres=gpu:1\n" +
-                # "#SBATCH --partition=gpu\n" +
-                "#SBATCH --partition=cpu_long\n" +
+                f"#SBATCH --partition={partition}\n" +
+                gpu_load +
                 "\n" +
                 "module purge\n" +
                 "module load anaconda3/2020.02/gcc-9.2.0\n"+
-                # "module load cuda/10.2.89/intel-19.0.3.199\n"+
+                conda_load +
                 "source activate pytorch_vnew\n" +
                 "cd ${SLURM_SUBMIT_DIR}\n\n")
     return command
@@ -75,37 +87,60 @@ def clean_command(job_name_long, exp_name):
     return command
 
 
-#if mode == "imputation":
-#    name_rt = f"FINETUNE_{pattern}_{d_model}_{d_ff}_{heads}_{layers}_{seq_len}"
-#    command_rt = "python src/main.py " +\
-#                 "--output_dir experiments " +\
-#                 f"--name {name_rt} " +\
-#                 "--task regression " +\
-#                 f"--data_dir {data_dir_retrain} " +\
-#                 "--data_class hur " +\
-#                 f"--pattern {pattern_rt} " +\
-#                 "--val_ratio 0.2 " +\
-#                 f"--epochs {epochs} " +\
-#                 "--optimizer RAdam " +\
-#                 f"--batch_size {batch_size} " +\
-#                 f"--d_model {d_model} " +\
-#                 f"--dim_feedforward {d_ff} " +\
-#                 f"--num_heads {heads} " +\
-#                 f"--num_layers {layers} " +\
-#                 f"--max_seq_len {seq_len} " +\
-#                 f"--load_model experiments/{name}/checkpoints/model_best.pth " +\
-#                 "--no_timestamp " +\
-#                 "--seed 1 " +\
-#                 "--change_output\n"
-#    command = command + command_rt
-#    name = name_rt
+def imputation_command(name, pattern, seq_len, val_ratio=0.2, epochs=2000, batch_size=32, d_model=16, d_ff=128, heads=4, layers=2, other_args=None):
+    command = "python src/main.py " +\
+              "--output_dir experiments " +\
+              f"--name {name} " +\
+              f"--task imputation " +\
+              f"--data_dir data/regression/ImputationHUR/ " +\
+              "--data_class hur " +\
+              f"--pattern {pattern} " +\
+              f"--val_ratio {val_ratio} " +\
+              f"--epochs {epochs} " +\
+              "--optimizer RAdam " +\
+              f"--batch_size {batch_size} " +\
+              f"--d_model {d_model} " +\
+              f"--dim_feedforward {d_ff} " +\
+              f"--num_heads {heads} " +\
+              f"--num_layers {layers} " +\
+              f"--max_seq_len {seq_len} " +\
+              "--no_timestamp " +\
+              "--normalization_layer LayerNorm " +\
+              "--seed 1 "
+
+    if other_args is not None:
+        for arg in other_args:
+            command += arg + " "
+    command += "\n"
+
+    return command
 
 
-#    with open(job_name, 'w') as fh:
-#        fh.write(start_script)
-#        fh.write(command)
-#        fh.write(command_2)
-#        fh.write(command_3)
+def retrain_command(name, imputation_name, pattern, seq_len, val_ratio=0.2, epochs=2000, batch_size=32, d_model=16, d_ff=128, heads=4, layers=2, other_args=None):
+    command = "python src/main.py " +\
+              "--output_dir experiments " +\
+              f"--name {name} " +\
+              "--task regression " +\
+              f"--data_dir data/regression/HUR/ " +\
+              "--data_class hur " +\
+              f"--pattern {pattern} " +\
+              f"--val_ratio {val_ratio} " +\
+              f"--epochs {epochs} " +\
+              "--optimizer RAdam " +\
+              f"--batch_size {batch_size} " +\
+              f"--d_model {d_model} " +\
+              f"--dim_feedforward {d_ff} " +\
+              f"--num_heads {heads} " +\
+              f"--num_layers {layers} " +\
+              f"--max_seq_len {seq_len} " +\
+              f"--load_model experiments/{imputation_name}/checkpoints/model_best.pth " +\
+              "--no_timestamp " +\
+              "--seed 1 " +\
+              "--change_output "
 
-    if launch:
-        os.system(f"sbatch {job_name}")
+    if other_args is not None:
+        for arg in other_args:
+            command += arg + " "
+    command += "\n"
+
+    return command
