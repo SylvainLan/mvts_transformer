b659ff081a8ffe866d96ce6c9d1e65bda9d48598
Sylvain Lannuzel <sylvain.lannuzel@cybeletech.com>
2021-09-09 17:03:22 +0200

    simplify create data

--- src/create_data.py
+++ src/create_data.py
@@ -1,20 +1,20 @@
+# J'ai pas besoin de test pcq je dois le faire à la toute fin
+
 import pandas as pd
 import argparse
 
 
 def _parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--cities_test", nargs="+", type=int, required=True)
-    parser.add_argument("--name", type=str, required=False, default="")
+    parser.add_argument("--name", type=str, required=True)
+    parser.add_argument("--cities_train", nargs="+", type=int, required=True)
     parser.add_argument("--cities_val", nargs="+", type=int, required=False, default=None)
-    parser.add_argument("--cities_train", nargs="+", type=int, required=False, default=None)
     args = parser.parse_args()
     return args
 
 if __name__ == "__main__":
     args = _parse()
     cities_train = args.cities_train
-    cities_test = args.cities_test
     cities_val = args.cities_val
     name = args.name
 
@@ -26,20 +26,19 @@ if __name__ == "__main__":
 
     df["date"] = pd.to_datetime(df["date"])
     df = df.set_index(["CITY_NAME", "date"])
-    if cities_val is None:
-        if cities_train is None:
-            raise AttributeError("au moins train ou val")
-        else:
-            cities_val = list(set(cities).difference([*cities_test, *cities_train]))
-            postfix = "SINGLE"
-    else:
-        if cities_train is None:
-            cities_train = list(set(cities).difference([*cities_test, *cities_val]))
-            postfix = ""
+    # if cities_val is None:
+    #     if cities_train is None:
+    #         raise AttributeError("au moins train ou val")
+    #     else:
+    #         cities_val = list(set(cities).difference([*cities_test, *cities_train]))
+    #         postfix = "SINGLE"
+    # else:
+    #     if cities_train is None:
+    #         cities_train = list(set(cities).difference([*cities_test, *cities_val]))
+    #         postfix = ""
 
     df_train = df.loc[cities_train]
-    df_val = df.loc[cities_val]
-    print(postfix)
-
-    df_train.to_csv(f"data/regression/HUR/HUR_{name}{postfix}TRAIN.csv")
-    df_val.to_csv(f"data/regression/HUR/HUR_{name}{postfix}VAL.csv")
+    df_train.to_csv(f"data/regression/HUR/HUR_{name}TRAIN.csv")
+    if cities_val is not None:
+        df_val = df.loc[cities_val]
+        # TODO
