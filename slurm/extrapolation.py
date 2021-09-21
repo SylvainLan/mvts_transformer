e02c0498347ceb175918c765470b869a6634c6c4
Sylvain Lannuzel <sylvain.lannuzel@cybeletech.com>
2021-09-21 09:57:37 +0200

    add args to extrapolation for HP search

--- slurm/extrapolation.py
+++ slurm/extrapolation.py
@@ -1,23 +1,20 @@
 import os
+import argparse
 from utils import start_script, train_command, eval_command, clean_command, create_command
 import itertools
 
 
-def make_slurm(station, exp_name, job_name_short, job_name_long, seq_len, d_model, val_contiguous=False, nlayers=2):
+def make_slurm(station, exp_name, job_name_short, job_name_long, seq_len, d_model, nlayers=2):
     pattern = f"{station}TRAIN"
     start = start_script(job_name=job_name_short)
-    if val_contiguous:
-        other_args = ["--val_continuous"]
-    else:
-        other_args = None
     cmd_create = create_command(station=station,
-                                name=station,
-                                split_dates=True)
+                                name=station)
     cmd1 = train_command(name=exp_name,
                          pattern=pattern,
                          seq_len=seq_len,
                          d_model=d_model,
                          layers=nlayers,
+                         batch_size=batch_size,
                          other_args=other_args)
     cmd2 = eval_command(name=exp_name,
                         train_pattern=pattern,
@@ -41,11 +38,15 @@ def make_slurm_split_val(station,
                          job_name_long,
                          seq_len,
                          d_model,
-                         nlayers=2,
-                         n_splits=4):
+                         nlayers,
+                         heads,
+                         batch_size,
+                         n_splits,
+                         epochs,
+                         ncpus):
     pattern = f"{station}TRAIN"
     val_pattern = f"{station}VAL"
-    start = start_script(job_name=job_name_short)
+    start = start_script(job_name=job_name_short, mode="cpu", ncpus=ncpus)
     cmd_create = create_command(station=station,
                                 name=station,
                                 n_splits=n_splits)
@@ -61,7 +62,10 @@ def make_slurm_split_val(station,
                              val_pattern=f"{val_pattern}{i}",
                              seq_len=seq_len,
                              d_model=d_model,
-                             layers=nlayers)
+                             layers=nlayers,
+                             heads=heads,
+                             batch_size=batch_size,
+                             epochs=epochs)
         cmd2 = eval_command(name=f"{exp_name}{i}",
                             train_pattern=f"{pattern}{i}",
                             seq_len=seq_len,
@@ -69,30 +73,51 @@ def make_slurm_split_val(station,
                             layers=nlayers)
         cmd3 = clean_command(job_name_long=jobs_name_long[i],
                              exp_name=f"{exp_name}_{i}",
-                             other_args=[f"rm data/regression/HUR/HUR_{pattern}{i}.csv",
-                                         f"rm data/regression/HUR/HUR_{val_pattern}{i}.csv"])
+                             )
         cmds_train.append(cmd1)
         cmds_eval.append(cmd2)
         cmds_clean.append(cmd3)
     for i in range(n_splits):
         with open(jobs_name_long[i], "w") as fh:
             fh.write(start)
-            fh.write(cmd_create)
+            #fh.write(cmd_create)
             fh.write(cmds_train[i])
             fh.write(cmds_eval[i])
             fh.write(cmds_clean[i])
 
 
+
+def _parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ncpus", type=int, default=1)
+    parser.add_argument("--nlayers", type=int, default=2)
+    parser.add_argument("--heads", type=int, default=4)
+    parser.add_argument("--d_model", type=int, default=32)
+    parser.add_argument("--seq_len", type=int, default=30)
+    parser.add_argument("--d_ff", type=int, default=64)
+    parser.add_argument("--batch_size", type=int, default=32)
+    args = parser.parse_args()
+    return args
+
+
 if __name__ == "__main__":
-    nlayers = 4
-    d_model = 64
-    seq_len = 60
-    d_ff = 64
-    n_splits = 4
+
+    args = _parse()
+
+    ncpus = args.ncpus
+    nlayers = args.nlayers
+    heads = args.heads
+    d_model = args.d_model
+    seq_len = args.seq_len
+    d_ff = args.d_ff
+    batch_size = args.batch_size
+
+    epochs = 1000
     cities = [19, 27, 34, 50, 77, 78, 84, 99]
+    n_splits = 3
     for c in cities:
         exp_name = f"{c}_extrapolation"
-        job_name_long = f"slurm/{c}_130921.slurm"
+        job_name_long = f"slurm/{c}_210921.slurm"
         make_slurm_split_val(station=c,
                              exp_name=exp_name,
                              job_name_short=f"{c}_extra",
@@ -100,7 +125,11 @@ if __name__ == "__main__":
                              seq_len=seq_len,
                              d_model=d_model,
                              nlayers=nlayers,
-                             n_splits=n_splits
+                             heads=heads,
+                             batch_size=batch_size,
+                             n_splits=n_splits,
+                             epochs=epochs,
+                             ncpus=ncpus,
                              )
 
         job_name_long = job_name_long.split(".slurm")[0]
