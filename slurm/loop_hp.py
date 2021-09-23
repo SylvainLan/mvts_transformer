41e2d103c240539f41dda4f5a00077d3d08e9961
Sylvain Lannuzel <sylvain.lannuzel@cybeletech.com>
2021-09-23 16:55:46 +0200

    new exps

--- slurm/loop_hp.py
+++ slurm/loop_hp.py
@@ -4,12 +4,19 @@ import argparse
 import glob
 
 
-nlayers = [2]
-heads = [4]
-d_model = [32]
-seq_len = [30]
-d_ff = [32, 64, 128]
+nlayers = [4]
+heads = [8]
+d_model = [64]
+seq_len = [60]
+d_ff = [128]
 batch_size = [32]
+
+nlayers = [2, 2, 2, 2, 2, 2, 4, 4]
+heads = [4, 4, 4, 4, 4, 8, 4, 8]
+d_model = [32, 32, 32, 32, 64, 32, 32, 64]
+seq_len = [30, 30, 30, 60, 30, 30, 30, 60]
+d_ff = [64, 64, 128, 64, 64, 64, 64, 128]
+batch_size = [32, 64, 32, 32, 32, 32, 32, 32]
 
 
 def _parse():
@@ -28,19 +35,25 @@ if __name__ == "__main__":
 
     if clean:
         print("cleaning")
-        for f in glob.glob("experiments/single_station/210921/exp*"):
+        for f in glob.glob("experiments/single_station/230921/exp*"):
             subprocess.call(["rm", "-r", "-f", f])
-        subprocess.call(["rm", "experiments/single_station/210921/README"])
+        subprocess.call(["rm", "experiments/single_station/230921/README"])
 
-    if i_max == 0 and len(glob.glob("experiments/single_station/210921/exp*")) > 1:
-            i_max = max([int(f.split("exp")[-1]) for f in glob.glob("experiments/single_station/210921/exp*")]) + 1
+    if i_max == 0 and len(glob.glob("experiments/single_station/230921/exp*")) > 1:
+            i_max = max([int(f.split("exp")[-1]) for f in glob.glob("experiments/single_station/230921/exp*")]) + 1
 
-    for i, (n, h, d, s, ff, b) in enumerate(itertools.product(nlayers,
-                                                             heads,
-                                                             d_model,
-                                                             seq_len,
-                                                             d_ff,
-                                                             batch_size)):
+    # for i, (n, h, d, s, ff, b) in enumerate(itertools.product(nlayers,
+    #                                                          heads,
+    #                                                          d_model,
+    #                                                          seq_len,
+    #                                                          d_ff,
+    #                                                          batch_size)):
+    for i, (n, h, d, s, ff, b) in enumerate(zip(nlayers,
+                                                heads,
+                                                d_model,
+                                                seq_len,
+                                                d_ff,
+                                                batch_size)):
         if mode == "ruche":
             subprocess.call(["python", "slurm/extrapolation.py",
                              "--nlayers", f"{n}",
@@ -51,6 +64,6 @@ if __name__ == "__main__":
                              "--batch_size", f"{b}",
                              "--exp_prefix", f"exp{i + i_max}"])
         else:
-            subprocess.call(["mkdir", f"experiments/single_station/210921/exp{i + i_max}"])
-            with open("experiments/single_station/210921/README", "a") as f:
+            subprocess.call(["mkdir", f"experiments/single_station/230921/exp{i + i_max}"])
+            with open("experiments/single_station/230921/README", "a") as f:
                 f.write(f"exp{i + i_max} : nlayers {n}, heads {h}, d_model {d}, seq_len {s}, d_ff {ff}, batch_size {b}\n")
