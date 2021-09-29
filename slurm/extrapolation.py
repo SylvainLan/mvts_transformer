6a8cb0d832262e971436da0cd66b4a226d08fbde
Sylvain Lannuzel <sylvain.lannuzel@cybeletech.com>
2021-09-29 17:05:57 +0200

    clean slurm

--- slurm/extrapolation.py
+++ slurm/extrapolation.py
@@ -2,34 +2,52 @@ import os
 import argparse
 from utils import start_script, train_command, eval_command, clean_command, create_command
 import itertools
+import glob
 
 
-# def make_slurm(station, exp_name, job_name_short, job_name_long, seq_len, d_model, nlayers=2):
-#     pattern = f"{station}TRAIN"
-#     start = start_script(job_name=job_name_short)
-#     cmd_create = create_command(station=station,
-#                                 name=station)
-#     cmd1 = train_command(name=exp_name,
-#                          pattern=pattern,
-#                          seq_len=seq_len,
-#                          d_model=d_model,
-#                          layers=nlayers,
-#                          batch_size=batch_size,
-#                          other_args=other_args)
-#     cmd2 = eval_command(name=exp_name,
-#                         train_pattern=pattern,
-#                         seq_len=seq_len,
-#                         d_model=d_model,
-#                         layers=nlayers)
-#     cmd3 = clean_command(job_name_long=job_name_long,
-#                          exp_name=exp_name,
-#                          other_args=[f"rm data/regression/HUR/HUR_{pattern}.csv"])
-#     with open(job_name_long, "w") as fh:
-#         fh.write(start)
-#         fh.write(cmd_create)
-#         fh.write(cmd1)
-#         fh.write(cmd2)
-#         fh.write(cmd3)
+def make_slurm(station,
+               exp_name,
+               job_name_short,
+               job_name_long,
+               seq_len,
+               d_model,
+               d_ff,
+               nlayers,
+               heads,
+               batch_size,
+               epochs,
+               ncpus,
+               n_splits=1):
+    pattern = f"{station}TRAIN"
+    start = start_script(job_name=job_name_short, mode="cpu", ncpus=ncpus)
+    cmd_create = create_command(station=station,
+                                name=station)
+    cmd1 = train_command(name=exp_name,
+                         pattern=pattern,
+                         seq_len=seq_len,
+                         d_model=d_model,
+                         d_ff=d_ff,
+                         layers=nlayers,
+                         heads=heads,
+                         batch_size=batch_size,
+                         epochs=epochs)
+    cmd2 = eval_command(name=exp_name,
+                        train_pattern=pattern,
+                        seq_len=seq_len,
+                        batch_size=batch_size,
+                        d_model=d_model,
+                        d_ff=d_ff,
+                        heads=heads,
+                        layers=nlayers)
+    cmd3 = clean_command(job_name_long=job_name_long,
+                         exp_name=exp_name,
+                         other_args=[f"rm data/regression/HUR/HUR_{pattern}.csv"])
+    with open(job_name_long, "w") as fh:
+        fh.write(start)
+        fh.write(cmd_create)
+        fh.write(cmd1)
+        fh.write(cmd2)
+        fh.write(cmd3)
 
 
 def make_slurm_split_val(station,
@@ -124,27 +142,34 @@ if __name__ == "__main__":
 
     epochs = 1000
     cities = [19, 27, 34, 50, 77, 78, 84, 99]
-    n_splits = 3
+    n_splits = 1
+
+    if n_splits > 1:
+        slurmer = make_slurm_split_val
+    else:
+        slurmer = make_slurm
 
 
     for c in cities:
         exp_name = f"{exp_prefix}{c}_extrapolation"
-        job_name_long = f"slurm/{exp_prefix}{c}_210921.slurm"
-        make_slurm_split_val(station=c,
-                             exp_name=exp_name,
-                             job_name_short=f"{c}_extra",
-                             job_name_long=job_name_long,
-                             seq_len=seq_len,
-                             d_model=d_model,
-                             d_ff=d_ff,
-                             nlayers=nlayers,
-                             heads=heads,
-                             batch_size=batch_size,
-                             n_splits=n_splits,
-                             epochs=epochs,
-                             ncpus=ncpus,
-                             )
+        job_name_long = f"slurm/{exp_prefix}{c}_290921.slurm"
+        slurmer(station=c,
+                exp_name=exp_name,
+                job_name_short=f"{c}_extra",
+                job_name_long=job_name_long,
+                seq_len=seq_len,
+                d_model=d_model,
+                d_ff=d_ff,
+                nlayers=nlayers,
+                heads=heads,
+                batch_size=batch_size,
+                n_splits=n_splits,
+                epochs=epochs,
+                ncpus=ncpus,
+                )
 
         job_name_long = job_name_long.split(".slurm")[0]
-        for i in range(n_splits):
-            os.system(f"sbatch {job_name_long}_{i}.slurm")
+        for f in glob.glob(f"{job_name_long}*.slurm"):
+        #for i in range(n_splits):
+            #os.system(f"sbatch {job_name_long}_{i}.slurm")
+            os.system(f"sbatch {f}")
